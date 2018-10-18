b5f94516c5397c593fce570f3c0371447eb9d4c1
EMMA BETHEL <emma.L.bethel@gmail.com>
2018-10-18 10:24:21 -0700

    fixed chick so one chick only makes one sound

--- Chick.java
+++ Chick.java
@@ -1,11 +1,10 @@
 class Chick implements Animal 
 {     
-     private String type, sound1,sound2;
+     private String type, sound;
      public Chick(String t, String s1,String s2){
      	type = t;
-     	sound1 = s1;
-     	sound2 = s2;
+     	sound = Math.random() < 0.5? s1:s2;
      } 
-     public String getSound(){return Math.random() * 2 < 0.5? sound1:sound2;}
+     public String getSound(){return sound;}
      public String getType(){return type;}
 }
